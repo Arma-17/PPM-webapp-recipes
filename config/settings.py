df2929b8fc98c8b3827b80239e4d01554046458a
Arma-17 <77484963+Arma-17@users.noreply.github.com>
2023-05-30 20:38:20 +0200

    New commit

--- config/settings.py
+++ config/settings.py
@@ -143,3 +143,5 @@ LOGIN_URL = 'user-login'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'users.UserProfile'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class UserProfile(AbstractUser):
+    followers = models.ManyToManyField('self', related_name='following', symmetrical=False)
+
+    def __str__(self):
+        return self.username
