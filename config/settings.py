c30a4bf31224b8e8454aff8754793ae2005f5a5e
Arma-17 <77484963+Arma-17@users.noreply.github.com>
2023-06-10 00:11:43 +0200

    Deploy

--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,10 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-3564.up.railway.app/',
+]
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -124,13 +128,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
-
-STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,"static")
-]
+STATIC_URL = '/static/'
+if DEBUG:
+  STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+  STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
