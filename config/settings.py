625a8fb36e5bedf577510c1f92676d53353e1abc
Arma-17 <77484963+Arma-17@users.noreply.github.com>
2023-06-09 23:53:57 +0200

    Deplying on railway

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-yxk(i=9k!^(qp(r)(#1*p2=$@*_3#3v_23n*))t8=49b(!$^f!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -125,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static")
 ]
