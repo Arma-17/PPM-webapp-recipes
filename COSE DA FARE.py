ac3519c4db81792c7385f62defe11e300d5e2ac1
Arma-17 <77484963+Arma-17@users.noreply.github.com>
2025-02-04 14:08:09 +0100

    nav bar customization

--- COSE DA FARE.py
+++ COSE DA FARE.py
@@ -1,29 +1,12 @@
 # T O D O di codice
 
-#TODO mettere la creazione delle ricette con categoria multi scelta quindi possono essere selezionate più categorie all'interno delle quali la ricetta viene posizionata
-
 #TODO mettere una sezione "cosa ho nel frigo dioboia?" inserendo gli ingredienti fa vedere le ricette che hanno solo quegli ingredienti
 
-#TODO fare la pagina che mostra la ricetta con la formattazione che ci serve quindi con gli ingredienti eccetera
+#TODO la ricerca va fatta
 
-#TODO fare  l'effettivo update, per ora mostra unicamente le cose che c'erano in precedenza, vogliamo abilitare la modifica a sto punto
-
-#TODO aggiungere come per ingredients anche per destription una cosa simile, ovvero si mette una description e di fianco un'immagine, senza però rendere obbligatorio l'ineserimento
-#dell'immagine in modo che si possano anche solo scrivere i passaggi, e deve funzionare essattamente come ingredients ovvero che si può rimuovere e 
-#aggiungere un campo (ogni campo è formato da decrizione e immagine). Inoltre per il salvataggio potrebbe essere conveniente fare come per gli ingredienti ovvero che concateniamo tutto 
 
 # T O D O di formattazione
 
-#TODO il bottone remove nella creazione/modifica di una ricetta non può essere pigiato se è il primo elemento, solo dal secondo in poi. 
-# Bisogna fare che lo si può rendere pigiabile se ci sono almeno due elementi
+#TODO migliorare la visualizzazione in tutto il sito
 
-#TODO il titolo delle ricette deve trovarsi prima dell'immagine quando siamo in visualizzazione
-
-#TODO la vista del delete è un pochino da rivedere che fa abbastanza rabbrividire
-
-#TODO modificare la dimenione della casella di testo negli step e rimuovere description
-#TODO asistemare le categorienell'hatml dell'update, non vengono visualizzate
-#TODO aggiungere per quante persone è il prodotto con 
-#todo mettere una sicura per il pulsante save quando aggiungi la ricetta
-#TODO mettere un elemento che mostri che il pulsante crop image è stato cliccato, nell' add recipe
-#TODO sistemare com'è posizionato lo slider delle persone che rompe il layout
+#TODO da rivedere il tasto per visualizzare le pagine nella home (next e previous) che fa abbastanza merda
